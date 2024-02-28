9f99c6b41d4461c22af11d86fa115b36ed7dbc01
DexDevNL2023 <dexternlang@gmail.com>
2024-02-28 20:02:45 +0100

    static landing is okay 23

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('./dist/'));
-
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/' });
-});
-
-app.listen(process.env.PORT || 8080);
-
-
-/*//Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -20,13 +8,13 @@ const app = express();
 //app.use(express.static('./dist/service-manager-app'));
 app.use(express.static(path.join(__dirname, 'dist','service-manager-app')));
 
-app.get('/!*', (req, res) =>
-    /!*res.sendFile('index.html', {root: 'dist/service-manager-app/'}),*!/
+app.get('/*', (req, res) =>
+    /*res.sendFile('index.html', {root: 'dist/service-manager-app/'}),*/
     res.sendFile(path.join(__dirname,'dist','service-manager-app','index.html'))
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);*/
+app.listen(process.env.PORT || 8080);
 
 
 
