c409fdd92cacdec52c9ecd73c7d07c4382b1ae83
DexDevNL2023 <dexternlang@gmail.com>
2024-02-28 19:48:54 +0100

    static landing is okay 21

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/service-manager-app'));
+//app.use(express.static('./dist/service-manager-app'));
+app.use(express.static(path.join(__dirname, 'dist','service-manager-app')));
 
 app.get('/*', (req, res) =>
-        res.sendFile('index.html', {root: 'dist/service-manager-app/'}),
+    /*res.sendFile('index.html', {root: 'dist/service-manager-app/'}),*/
+    res.sendFile(path.join(__dirname,'dist','service-manager-app','index.html'))
 );
 
 // Start the app by listening on the default Heroku port
