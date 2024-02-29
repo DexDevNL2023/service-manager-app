9019e61c24f10a6b36da81110f9546999c4f078a
DexDevNL2023 <dexternlang@gmail.com>
2024-02-29 09:36:12 +0100

    static landing is okay 29

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-//Install express server
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/dist/service-manager-app'))
+
+app.get('/ping', function (req, res)
+{
+    return res.send('pong');
+});
+
+app.get('/*', function (req, res)
+{
+    res.sendFile(path.join(__dirname + '/dist/service-manager-app/index.html'));
+});
+
+app.listen(port, function ()
+{
+    console.info('Angular Server App listening on port ' + port);
+});
+
+
+
+/*//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -8,13 +33,13 @@ const app = express();
 //app.use(express.static('./dist/service-manager-app'));
 app.use(express.static(path.join(__dirname, 'dist','service-manager-app')));
 
-app.get('/*', (req, res) =>
-    /*res.sendFile('index.html', {root: 'dist/service-manager-app/'}),*/
+app.get('/!*', (req, res) =>
+    /!*res.sendFile('index.html', {root: 'dist/service-manager-app/'}),*!/
     res.sendFile(path.join(__dirname,'dist','service-manager-app','index.html'))
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);*/
 
 
 
